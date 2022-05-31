011b8096575da3259a95e0d1cca8e8ad149e9f7e
Eduardo Martins <dudumartinss2004@gmail.com>
2022-05-31 11:20:43 -0300

    Commit 31/05
    Foi mostrado outra maneira de navegar entre as intents

--- ConceitosIntent/app/src/main/java/formacao/desenvolvedores/tecnologia/uno/porjetodesoftwareorientadoaobjetos/conceitosintent/MainActivity.java
+++ ConceitosIntent/app/src/main/java/formacao/desenvolvedores/tecnologia/uno/porjetodesoftwareorientadoaobjetos/conceitosintent/MainActivity.java
@@ -1,9 +1,15 @@
 package formacao.desenvolvedores.tecnologia.uno.porjetodesoftwareorientadoaobjetos.conceitosintent;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,19 +26,19 @@ public class MainActivity extends AppCompatActivity {
     private EditText edtPergunta;
     private ImageButton btnDelete;
     public static final int REQUEST_CODE = 5;
-
+    private ActivityResultLauncher<Intent> activityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         btnPerguntar    = findViewById(R.id.btnPerguntar);
         tvExibirRespota = findViewById(R.id.tvExibirResposta);
         edtPergunta     = findViewById(R.id.edtPergunta);
         btnDelete       = findViewById(R.id.btnDelete);
         tvTitulo        = findViewById(R.id.tvTitulo);
+
 
         tvTitulo.setVisibility(View.INVISIBLE);
 
@@ -41,12 +47,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(!edtPergunta.getText().toString().isEmpty()){
-                    Intent irParaOutraActivity = new Intent(MainActivity.this, RespostaActivity.class);
 
-                    String conteudo = edtPergunta.getText().toString();
-                    irParaOutraActivity.putExtra("Pergunta", conteudo);
+                    if(Build.VERSION.SDK_INT < Build.VERSION_CODES.R){
+                        Intent irParaOutraActivity = new Intent(MainActivity.this, RespostaActivity.class);
 
-                    startActivityForResult(irParaOutraActivity, REQUEST_CODE);
+                        String conteudo = edtPergunta.getText().toString();
+                        irParaOutraActivity.putExtra("Pergunta", conteudo);
+
+                        startActivityForResult(irParaOutraActivity, REQUEST_CODE);
+
+                    } else {
+                        openActivityForResult();
+                    }
+
 
 
                 } else {
@@ -63,9 +76,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
+        //Novo jeito em execução
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK){
+                    Intent data = result.getData();
+                    //tvExibirRespota.setText(data.getExtras().toString());
+                    tvExibirRespota.setText(data.getExtras().getString("returnData"));
+                    tvTitulo.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -74,8 +100,6 @@ public class MainActivity extends AppCompatActivity {
         if((requestCode == REQUEST_CODE) && (resultCode == RESULT_OK)){
 
             String returnString = data.getExtras().getString("returnData");
-
-
 
             if(returnString != null){
                 if(!returnString.isEmpty()){
@@ -88,4 +112,14 @@ public class MainActivity extends AppCompatActivity {
             tvExibirRespota.setText(returnString);
         }
     }
+
+    //Novo jeito em execução
+    private void openActivityForResult(){
+        Intent intent2 = new Intent(this, RespostaActivity.class);
+
+        intent2.putExtra("Pergunta", edtPergunta.getText().toString());
+
+        activityResultLauncher.launch(intent2);
+    }
+
 }
