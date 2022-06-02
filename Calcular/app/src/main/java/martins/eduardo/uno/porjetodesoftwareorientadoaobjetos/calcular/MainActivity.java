e4697233bc0caaaf9a33f3724a2d674292f4fa75
Eduardo-Martins04 <dudumartinss2004@gmail.com>
2022-06-02 11:21:01 -0300

    commit 02/06/2022
    Foi feito o layout do programa Calcular

--- Calcular/app/src/main/java/martins/eduardo/uno/porjetodesoftwareorientadoaobjetos/calcular/MainActivity.java
+++ Calcular/app/src/main/java/martins/eduardo/uno/porjetodesoftwareorientadoaobjetos/calcular/MainActivity.java
@@ -7,13 +7,12 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "teste";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG, "onCreate: ");
     }
 }
