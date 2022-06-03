bb8a16613acc649b68e12e2393b7538cf1c8b2e1
Eduardo-Martins04 <dudumartinss2004@gmail.com>
2022-06-03 08:23:44 -0300

    Commit 1
    Prova 03/06/2022

---
+++ Prova1EduardoMauricio/app/src/main/java/martins/eduardo/uno/prova1eduardomauricio/MainActivity.java
@@ -0,0 +1,14 @@
+package martins.eduardo.uno.prova1eduardomauricio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
