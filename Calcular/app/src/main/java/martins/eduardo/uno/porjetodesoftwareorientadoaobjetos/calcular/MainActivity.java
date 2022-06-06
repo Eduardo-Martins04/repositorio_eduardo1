2a99a8f6bd2465116af69f7c14dba70befba48f6
Eduardo-Martins04 <dudumartinss2004@gmail.com>
2022-06-06 09:56:21 -0300

    commit 06/06
    Dando continuidade no projeto Calcular

--- Calcular/app/src/main/java/martins/eduardo/uno/porjetodesoftwareorientadoaobjetos/calcular/MainActivity.java
+++ Calcular/app/src/main/java/martins/eduardo/uno/porjetodesoftwareorientadoaobjetos/calcular/MainActivity.java
@@ -4,8 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String DIVISAO = "divisao";
+    private Spinner spiOpcoes;
+    private EditText edtNumero1, edtNumero2;
+    private ImageView imgOperacao;
+    private TextView tvResultado;
+    private Button btnCalcular;
 
 
 
@@ -13,6 +26,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spiOpcoes = findViewById(R.id.spiOpcoes);
+        edtNumero1 = findViewById(R.id.edtNumero1);
+        edtNumero2 = findViewById(R.id.edtNumero2);
+        imgOperacao = findViewById(R.id.imgOperacao);
+        btnCalcular = findViewById(R.id.btnCalcular);
+        tvResultado = findViewById(R.id.tvResultado);
+
+        //PEGA A OPCAO SELECIONADA DO SPINNER
+        String operacaoSelecionada = spiOpcoes.getSelectedItem().toString();
+
+        ArrayAdapter<String> adapterOperacoes = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item
+                , getResources().getStringArray(R.array.operacoes_matematica));
+        adapterOperacoes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spiOpcoes.setAdapter(adapterOperacoes);
+
+        if(operacaoSelecionada == DIVISAO){// == "divisao"
+            
+        }
 
     }
 }
