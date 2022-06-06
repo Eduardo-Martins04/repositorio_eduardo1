a4d898e2474f6f1e496f42a7d64c0fc6059dd5f9
Eduardo-Martins04 <dudumartinss2004@gmail.com>
2022-06-06 11:13:55 -0300

    commit 06/06/2022

--- Calcular/app/src/main/java/martins/eduardo/uno/porjetodesoftwareorientadoaobjetos/calcular/MainActivity.java
+++ Calcular/app/src/main/java/martins/eduardo/uno/porjetodesoftwareorientadoaobjetos/calcular/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,9 +14,13 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String DIVISAO = "divisao";
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    public static final String DIVISAO = "Dividir";
+    public static final String MULTIPLICAR = "Multiplicar";
+    public static final String SOMA = "Somar";
+    public static final String SUBTRAIR = "Subtrair";
     private Spinner spiOpcoes;
     private EditText edtNumero1, edtNumero2;
     private ImageView imgOperacao;
@@ -27,15 +34,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spiOpcoes = findViewById(R.id.spiOpcoes);
-        edtNumero1 = findViewById(R.id.edtNumero1);
-        edtNumero2 = findViewById(R.id.edtNumero2);
+        spiOpcoes   = findViewById(R.id.spiOpcoes);
+        edtNumero1  = findViewById(R.id.edtNumero1);
+        edtNumero2  = findViewById(R.id.edtNumero2);
         imgOperacao = findViewById(R.id.imgOperacao);
         btnCalcular = findViewById(R.id.btnCalcular);
         tvResultado = findViewById(R.id.tvResultado);
 
-        //PEGA A OPCAO SELECIONADA DO SPINNER
-        String operacaoSelecionada = spiOpcoes.getSelectedItem().toString();
+        imgOperacao.setVisibility(View.INVISIBLE);
+
 
         ArrayAdapter<String> adapterOperacoes = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item
@@ -43,10 +50,43 @@ public class MainActivity extends AppCompatActivity {
         adapterOperacoes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spiOpcoes.setAdapter(adapterOperacoes);
+        spiOpcoes.setOnItemSelectedListener(this);//this usada como argumento de metodo
 
-        if(operacaoSelecionada == DIVISAO){// == "divisao"
-            
-        }
+        //PEGA A OPCAO SELECIONADA DO SPINNER
+        String operacaoSelecionada = spiOpcoes.getSelectedItem().toString();
+
+       btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(operacaoSelecionada == DIVISAO){// == "Dividir"
+                    Toast.makeText(MainActivity.this, "DIVISAO", Toast.LENGTH_SHORT).show();
+          
+                }
+
+                else if(operacaoSelecionada == MULTIPLICAR){// == "Multiplicar"
+
+                }
+
+                else if(operacaoSelecionada == SOMA){// == "Somar"
+
+                }
+
+                else if (operacaoSelecionada == SUBTRAIR){// == "Subtrair"
+
+                }
+
+            }
+       });
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
