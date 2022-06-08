51294ee37e9199c66615c6e3feb0baa14ccefdf2
Eduardo-Martins04 <dudumartinss2004@gmail.com>
2022-06-08 10:10:30 -0300

    Commit 08/06/2022
    Adicionando métodos para verificar as entradas de dados

--- Calcular/app/src/main/java/martins/eduardo/uno/porjetodesoftwareorientadoaobjetos/calcular/MainActivity.java
+++ Calcular/app/src/main/java/martins/eduardo/uno/porjetodesoftwareorientadoaobjetos/calcular/MainActivity.java
@@ -21,9 +21,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public static final String MULTIPLICAR = "Multiplicar";
     public static final String SOMAR = "Somar";
     public static final String SUBTRAIR = "Subtrair";
+    private int ZERO = 0;
     private Spinner spiOpcoes;
     private EditText edtNumero1, edtNumero2;
-    private ImageView imgOperacao;
+    private ImageView imgOperacao, imgLimpar;
     private TextView tvResultado;
     private Button btnCalcular;
 
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         edtNumero1  = findViewById(R.id.edtNumero1);
         edtNumero2  = findViewById(R.id.edtNumero2);
         imgOperacao = findViewById(R.id.imgOperacao);
+        imgLimpar   = findViewById(R.id.imgLimpar);
         btnCalcular = findViewById(R.id.btnCalcular);
         tvResultado = findViewById(R.id.tvResultado);
 
@@ -57,21 +59,44 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
        btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                /*if()*/
                 //PEGA A OPCAO SELECIONADA DO SPINNER
                 String operacaoSelecionada = spiOpcoes.getSelectedItem().toString();
 
-                if(operacaoSelecionada == DIVIDIR){// == "Dividir"
-                    tvResultado.setText("teste");
+                if(operacaoSelecionada.equals(DIVIDIR)){// == "Dividir"
+                    if(validarTermoVazio()){
+                        if (verificaDivisao0()){
+                            tvResultado.setText(dividir());
+                        } else {
+                            Toast.makeText(MainActivity.this, "O divisor não pode ser ZERO!", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(MainActivity.this, "Insira 2 numeros para realizar a divisão", Toast.LENGTH_SHORT).show();
+                    }
 
-                } else if(operacaoSelecionada == MULTIPLICAR){// == "Multiplicar"
+                } else if(operacaoSelecionada.equals(MULTIPLICAR)){// == "Multiplicar"
+                    if(validarTermoVazio()){
+                        tvResultado.setText(multiplicar());
+                    } else {
+                        Toast.makeText(MainActivity.this, "Insira 2 numeros para realizar a multiplicação", Toast.LENGTH_SHORT).show();
+                    }
 
-                } else if(operacaoSelecionada == SOMAR){// == "Somar"
+                } else if(operacaoSelecionada.equals(SOMAR)){// == "Somar"
+                    if(validarTermoVazio()){
+                        tvResultado.setText(somar());
+                    } else {
+                        Toast.makeText(MainActivity.this, "Insira 2 numeros para realizar a soma", Toast.LENGTH_SHORT).show();
+                    }
 
-                } else if (operacaoSelecionada == SUBTRAIR){// == "Subtrair"
 
+                } else if (operacaoSelecionada.equals(SUBTRAIR)){// == "Subtrair"
+                    if(validarTermoVazio()){
+                        tvResultado.setText(subtrair());
+                    } else {
+                        Toast.makeText(MainActivity.this, "Insira 2 numeros para realizar a subtração", Toast.LENGTH_SHORT).show();
+                    }
                 } else{
-
+                    Toast.makeText(MainActivity.this, "Selecione uma operação matematica", Toast.LENGTH_SHORT).show();
                 }
 
                 //tvResultado.setText(operacaoSelecionada);
@@ -79,33 +104,50 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
        });
 
+       imgLimpar.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               edtNumero1.setText("");
+               edtNumero2.setText("");
+               tvResultado.setText("");
+               //imgOperacao.setVisibility(View.INVISIBLE);
+           }
+       });
+
     }
+
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
         //Toast.makeText(MainActivity.this, adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
 
-
         if(adapterView.getItemAtPosition(i).toString().equals(DIVIDIR)){
             imgOperacao.setImageDrawable(getResources().getDrawable(R.drawable.divisao, getTheme()));
             imgOperacao.setVisibility(View.VISIBLE);
+            edtNumero1.setHint("Dividendo");
+            edtNumero2.setHint("Divisor");
 
         } else if (adapterView.getItemAtPosition(i).toString().equals(MULTIPLICAR)){
             imgOperacao.setImageDrawable(getResources().getDrawable(R.drawable.multiplica, getTheme()));
             imgOperacao.setVisibility(View.VISIBLE);
+            edtNumero1.setHint("Multiplicando");
+            edtNumero2.setHint("Multiplicador");
 
         } else if(adapterView.getItemAtPosition(i).toString().equals(SOMAR)){
             imgOperacao.setImageDrawable(getResources().getDrawable(R.drawable.soma, getTheme()));
             imgOperacao.setVisibility(View.VISIBLE);
+            edtNumero1.setHint("Parcela");
+            edtNumero2.setHint("Parcela");
+
 
         } else if(adapterView.getItemAtPosition(i).toString().equals(SUBTRAIR)){
             imgOperacao.setImageDrawable(getResources().getDrawable(R.drawable.subtracao, getTheme()));
             imgOperacao.setVisibility(View.VISIBLE);
-
+            edtNumero1.setHint("Minuendo");
+            edtNumero2.setHint("Subtraendo");
 
         }
-
 
     }
 
@@ -113,4 +155,76 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+    private String dividir() {
+
+            int n1 = Integer.valueOf(edtNumero1.getText().toString()).intValue();
+            int n2 = Integer.valueOf(edtNumero2.getText().toString()).intValue();
+
+            int res = n1 / n2;
+
+            return "O resultado da divisão é " + res;
+    }
+
+    private String multiplicar() {
+            int n1 = Integer.valueOf(edtNumero1.getText().toString()).intValue();
+            int n2 = Integer.valueOf(edtNumero2.getText().toString()).intValue();
+
+            int res = n1 * n2;
+
+            return "O resultado da multiplicação é " + res;
+
+    }
+
+    private String somar(){
+            int n1 = Integer.valueOf(edtNumero1.getText().toString()).intValue();
+            int n2 = Integer.valueOf(edtNumero2.getText().toString()).intValue();
+
+            int res = n1 + n2;
+
+            //return String.valueOf(res);
+            return "O resultado da soma é " + res;
+
+    }
+
+    private String subtrair(){
+        if(!edtNumero1.getText().toString().isEmpty() || !edtNumero2.getText().toString().isEmpty()){
+            int n1 = Integer.valueOf(edtNumero1.getText().toString()).intValue();
+            int n2 = Integer.valueOf(edtNumero2.getText().toString()).intValue();
+
+            int res = n1 - n2;
+
+            //return String.valueOf(res);
+            return "O resultado da subtração é " + res;
+        } else {
+            Toast.makeText(this, "Informe 2 numeros para realizar a subtração", Toast.LENGTH_SHORT).show();
+            return "";
+        }
+
+    }
+
+    private boolean validarTermoVazio(){
+        if(!edtNumero1.getText().toString().isEmpty()){
+            if (!edtNumero2.getText().toString().isEmpty()){
+                return true;
+            } else {
+                edtNumero2.requestFocus();
+                return false;
+            }
+        } else {
+            edtNumero1.requestFocus();
+            return false;
+        }
+    }
+
+    private boolean verificaDivisao0(){
+        int n2 = Integer.valueOf(edtNumero2.getText().toString()).intValue();
+        if(n2 != ZERO){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
