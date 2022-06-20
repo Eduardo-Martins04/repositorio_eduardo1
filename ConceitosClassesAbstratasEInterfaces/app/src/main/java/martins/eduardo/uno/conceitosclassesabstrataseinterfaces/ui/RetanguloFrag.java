1bf7ac2ae51ee1e11bda318ef07534a5596df576
Eduardo-Martins04 <dudumartinss2004@gmail.com>
2022-06-20 11:10:48 -0300

    Commit 20/06/2022
    Finalizando os conceitos de fragments

--- ConceitosClassesAbstratasEInterfaces/app/src/main/java/martins/eduardo/uno/conceitosclassesabstrataseinterfaces/ui/RetanguloFrag.java
+++ ConceitosClassesAbstratasEInterfaces/app/src/main/java/martins/eduardo/uno/conceitosclassesabstrataseinterfaces/ui/RetanguloFrag.java
@@ -7,11 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 import martins.eduardo.uno.conceitosclassesabstrataseinterfaces.R;
+import martins.eduardo.uno.conceitosclassesabstrataseinterfaces.formas.Retangulo;
 
 
 public class RetanguloFrag extends Fragment {
+    private EditText edtBase, edtAltura;
+    private TextView tvAreaRetangulo;
+    private ImageButton imgbLimpar, imgbCalcular;
 
     public RetanguloFrag() {
         // Required empty public constructor
@@ -29,6 +39,34 @@ public class RetanguloFrag extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View layoutFragment = inflater.inflate(R.layout.fragment_retangulo, container, false);
+
+        edtBase         = layoutFragment.findViewById(R.id.edtBase);
+        edtAltura       = layoutFragment.findViewById(R.id.edtLado2);
+        imgbCalcular    = layoutFragment.findViewById(R.id.imgbCalcularRetangulo);
+        imgbLimpar      = layoutFragment.findViewById(R.id.imgbLimparRetangulo);
+        tvAreaRetangulo = layoutFragment.findViewById(R.id.tvAreaRetangulo);
+
+        imgbCalcular.setOnClickListener(view -> {
+            if(edtBase.getText().toString().isEmpty()){
+                Toast.makeText(getContext(), "Informe a base do retângulo!", Toast.LENGTH_SHORT).show();
+
+            } else if(edtAltura.getText().toString().isEmpty()){
+                Toast.makeText(getContext(), "Informe a altura do retângulo!", Toast.LENGTH_SHORT).show();
+
+            } else{
+                Retangulo retangulo = new Retangulo(Double.valueOf(edtBase.getText().toString())
+                        , Double.valueOf(edtAltura.getText().toString()));
+
+                String areaFormatada = new DecimalFormat("#,##0.00").format(retangulo.area());
+                tvAreaRetangulo.setText(areaFormatada);
+            }
+        });
+
+        imgbLimpar.setOnClickListener(view -> {
+            edtBase.setText("");
+            edtAltura.setText("");
+            tvAreaRetangulo.setText("");
+        });
 
         return layoutFragment;
     }
