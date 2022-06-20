70c75cf16cf2710ca1f0d0f32623024de788e78b
Eduardo-Martins04 <dudumartinss2004@gmail.com>
2022-06-20 08:36:32 -0300

    commit 20/06/2022

--- ConceitosClassesAbstratasEInterfaces/app/src/main/java/martins/eduardo/uno/conceitosclassesabstrataseinterfaces/formas/Retangulo.java
+++ ConceitosClassesAbstratasEInterfaces/app/src/main/java/martins/eduardo/uno/conceitosclassesabstrataseinterfaces/formas/Retangulo.java
@@ -4,18 +4,18 @@ import martins.eduardo.uno.conceitosclassesabstrataseinterfaces.formas.Forma;
 
 public class Retangulo extends Forma {
     public Retangulo(double altura, double largura) {
-        super(2); // uso obrigat�rio do construtor da superclasse
+        super(2); // uso obrigatorio do construtor da superclasse
         setMedida(0, altura); // ajuste da medida da altura
         setMedida(1, largura); // ajuste da medida da largura
     }
 
-    // implementa��o do método abstrato area()
+    // implementação do método abstrato area()
     @Override
     public double area() {
         return getMedida(0) * getMedida(1);
     }
 
-    // implementa��o de opera��o espec�fica extra
+    // implementação de operacao especifica extra
     public double perimetro() {
         return 2 * (getMedida(0) + getMedida(1));
     }
