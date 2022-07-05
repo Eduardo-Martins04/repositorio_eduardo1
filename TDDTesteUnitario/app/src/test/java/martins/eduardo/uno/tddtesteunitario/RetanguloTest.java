18a6fe105c432767d5bdacae72c5dde0225d27e2
Eduardo-Martins04 <dudumartinss2004@gmail.com>
2022-07-05 09:13:29 -0300

    Commit 05/07/2022

--- TDDTesteUnitario/app/src/test/java/martins/eduardo/uno/tddtesteunitario/RetanguloTest.java
+++ TDDTesteUnitario/app/src/test/java/martins/eduardo/uno/tddtesteunitario/RetanguloTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 
+
 import martins.eduardo.uno.tddtesteunitario.forma.Retangulo;
 
 public class RetanguloTest {
@@ -14,7 +15,7 @@ public class RetanguloTest {
     @Test
     public void calcularAreaDoRetangulo(){
         double area_BaseXAltura = 10;
-        int PRECISAO_4_CASAS_DECIMAIS = 4;
+        double DIFERENCA_ACEITAVEL = 0.0001;
 
         retangulo.setMedidas(0, 5);
         retangulo.setMedidas(1, 2);
@@ -23,27 +24,25 @@ public class RetanguloTest {
         //double alturar = retangulo.getMedidas(1);
         double area = retangulo.getMedidas(0) * retangulo.getMedidas(1);
 
-        
-
-        /*assertEquals("A area de uma de um retangulo: " + area_BaseXAltura, area_BaseXAltura,
-                area, PRECISAO_4_CASAS_DECIMAIS);*/
-        Double calculoPapel = new Double(area_BaseXAltura);
+        assertEquals("A area de um retangulo: " + area_BaseXAltura, area_BaseXAltura,
+                area, DIFERENCA_ACEITAVEL);
+        /*Double calculoPapel = new Double(area_BaseXAltura);
         boolean x = calculoPapel.equals(new Double(area));
-        assertTrue(x);
+        assertTrue(x);*/
 
     }
 
     @Test
     public void calcularPerimetroRetangulo(){
-        double perimetro_Base5Altura2 = 20;
-        int PRECISAO_4_CASAS_DECIMAIS = 4;
+        double perimetro_BaseXAlturaY = 20;
+        double DIFERENCA_ACEITAVEL = 0.0001;
 
-        retangulo.setMedidas(0, 5);
-        retangulo.setMedidas(1, 2);
+        retangulo.setMedidas(0, 2);
+        retangulo.setMedidas(1, 5);
 
         double perimetro = retangulo.area() * 2;
 
-        assertEquals("O perimetro de um retangulo de base 5 e altura 2 é: 20", perimetro_Base5Altura2,
-                perimetro, PRECISAO_4_CASAS_DECIMAIS);
+        assertEquals("O perimetro de um retangulo de base 5 e altura 2 é: 20", perimetro_BaseXAlturaY,
+                perimetro, DIFERENCA_ACEITAVEL);
     }
 }
